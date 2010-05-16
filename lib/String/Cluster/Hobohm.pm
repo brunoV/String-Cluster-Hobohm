f206685c17ac6bb9087762f345728c60abcaf78e
Bruno Vecchi <vecchi.b@gmail.com>
2010-05-16 00:48:32 -0300

    initial commit -- basically working

---
+++ lib/String/Cluster/Hobohm.pm
@@ -0,0 +1,126 @@
+package String::Cluster::Hobohm;
+
+# ABSTRACT: Cluster strings using the Hobohm algorithm
+
+use v5.10.1;
+use strict;
+use warnings;
+
+use Mouse;
+use String::Cluster::Hobohm::Types 'Quotient';
+use Carp 'croak';
+use Text::LevenshteinXS;
+
+# accept a list of strings or a closure that returns, stepwise, the
+# strings (this prevents loading everything into memory in the case that
+# the dataset is too big)
+
+# Return, for now, a structure with the indices
+# {
+#    index1 => [ index2, index3, index4 ]
+#    ...
+# }
+
+has similarity => ( is => 'ro', default => 0.62, isa => Quotient );
+
+sub cluster {
+    my ($self, $sequences) = @_;
+    $sequences // croak "Need sequences as argument";
+
+    my @clusters;
+
+    foreach my $sequence (@$sequences) {
+
+        my $cluster_id = $self->_is_similar( $sequence, \@clusters );
+
+        if ( defined $cluster_id ) {
+            push @{ $clusters[ $cluster_id ] }, \$sequence;
+        }
+        else {
+            push @clusters, [ \$sequence ]
+        }
+    }
+
+    return \@clusters;
+}
+
+sub _is_similar {
+    my ($self, $sequence, $clusters ) = @_;
+
+    foreach my $i (0 .. $#$clusters) {
+        my $similarity = $self->_similarity(\$sequence, $clusters->[$i][0]);
+
+        return $i if $similarity >= $self->similarity;
+    }
+
+    return;
+}
+
+sub _similarity {
+    my $self = shift;
+
+    my @seqs = map { $$_ } @_;
+
+    my $distance = Text::LevenshteinXS::distance(@seqs)
+      // croak "unable to compute distance";
+
+    return 1 - $distance / length( $seqs[0] );
+}
+
+__PACKAGE__->meta->make_immutable;
+
+=head1 SYNOPSIS
+
+    use String::Cluster::Hobohm;
+
+    my @strings = qw(foo foa bar);
+
+    my $clusterer = String::Clusterer::Hobohm->new( similarity => 0.62 );
+
+    my $groups = $clusterer->cluster( \@strings );
+
+    # [ [ \'foo', \'foa' ], [ \'bar' ] ];
+
+    my @reduced = map { ${ $_->[0] } } @$groups;
+
+    # [ 'foo', 'bar' ];
+
+=head1 DESCRIPTION
+
+String::Clusterer::Hobohm implements the Hobohm clustering algorithm
+[1], originally devised to reduce redundancy of biological sequence data
+sets.
+
+As a clustering algorithm, it takes a set of sequences, and returns them
+grouped by similarity. The latter is computed using the Levenshtein
+distance, as implemented by the C<Text::LevenshteinXS> module.
+
+=attr similarity
+
+The similarity threshold that defines whether two strings are
+sufficiently alike to be part of the same cluster. Should be a number
+between 0 and 1. Defaults to 0.62.
+
+=method cluster
+
+    my $grouped = $hobohm->cluster( \@strings );
+
+Takes an array reference with the sequences to cluster as argument, and
+returns an array reference of clusters. Each cluster is depicted as a
+list of references to the strings that define it.
+
+For example, given the following array of strings, and a similarity of
+0.62:
+
+    [ 'foo', 'foa', 'bar' ];
+
+The data structure returned after clustering would be:
+
+    [ [ \'foo', \'foa' ], [ \'bar' ] ];
+
+The reason to use references instead of the actual strings is to avoid
+copying potentially large strings and taking up too much memory
+(remember that the algorithm was designed with biological sequences in
+mind).
+
+=cut
---
+++ lib/String/Cluster/Hobohm/Types.pm
@@ -0,0 +1,10 @@
+package String::Cluster::Hobohm::Types;
+
+# ABSTRACT: Specific types for String::Cluster::Hobohm;
+
+use MouseX::Types -declare => ['Quotient'];
+use MouseX::Types::Mouse 'Num';
+
+subtype Quotient, as Num, where { $_ >= 0 && $_ <= 1 };
+
+1;
